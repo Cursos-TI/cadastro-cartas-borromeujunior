2347cd0ebec4d2914cbb03035c65077cb6217103
borromeujunior <borromeujunior@hotmail.com>
2025-09-16 02:12:48 +0000

    Erro do scanf no item: Pontos turisticos

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,53 +6,55 @@
 
 int main() {
 // Área para definição das variáveis para armazenar as propriedades das cidades
-char nomeCidade1[50], nomeCidade2[50], letraEstado1[2], letraEstado2[2], 
-int populacao1, populacao2, pontos1, pontos2; codigoCarta1[4], codigoCarta2[4];
-float area1, area2, pib1, pib2;
+    char nomeCidade1[50], nomeCidade2[50], letraEstado1, letraEstado2, codigoCarta1[5], codigoCarta2[5];
+    int populacao1, populacao2, pontos1, pontos2;
+    float area1, area2, pib1, pib2;
   
 // Área para entrada de dados da carta 1
-printf("Digite a letra do estado: (A-H)");
-scanf("%s", letraEstado1);
 
-printf("Digite o número do estado: (01-04)\n");
-scanf("%d", codigoCarta1);
+printf("Cadastre os dados da Carta 1:\n");
+printf("Digite a letra do estado: (A-H)");
+scanf("%c", &letraEstado1);
+
+printf("Digite o código para a Carta (ex: A01,B03): \n");
+scanf("%s", &codigoCarta1);
 
 printf("Digite o nome da cidade: \n");
-scanf("%s", nomeCidade1);
+scanf("%s", &nomeCidade1);
 
 printf("Digite a população da cidade: \n");
-scanf("%d", populacao1);
+scanf("%d", &populacao1);
 
 printf("Digite a área total da cidade: \n");
-scanf("%f", area1);
+scanf("%f", &area1);
 
 printf("Digite o PIB da cidade: \n");
-scanf("%f", pib1);
+scanf("%f", &pib1);
 
 printf("Digite o numero de pontos turísticos da cidade: \n")
-scanf("%d", pontos 1);
+scanf("%d", &pontos1);
 
 // Área para entrada de dados da carta 2
 printf("Digite a letra do estado: (A-H)");
 scanf("%s", letraEstado2);
 
 printf("Digite o número do estado: (01-04)\n");
-scanf("%d", codigoCarta2);
+scanf("%d", &codigoCarta2);
 
 printf("Digite o nome da cidade: \n");
-scanf("%s", nomeCidade2);
+scanf("%s", &nomeCidade2);
 
 printf("Digite a população da cidade: \n");
-scanf("%d", populacao2);
+scanf("%d", &populacao2);
 
 printf("Digite a área total da cidade: \n");
-scanf("%f", area2);
+scanf("%f", &area2);
 
 printf("Digite o PIB da cidade: \n");
-scanf("%f", pib2);
+scanf("%f", &pib2);
 
 printf("Digite o numero de pontos turísticos da cidade: \n")
-scanf("%d", pontos 2);
+scanf("%d", &pontos2);
   
 
 // Área para exibição dos dados da cidade
